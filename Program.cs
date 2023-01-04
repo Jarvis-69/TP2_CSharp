17230842fc6a387d35b64814ce5a612d57dd0b8b
Jarvis-69 <94354305+Jarvis-69@users.noreply.github.com>
2023-01-04 15:18:08 +0100

    Exo4

--- Program.cs
+++ Program.cs
@@ -3,36 +3,31 @@ using System;
 
 namespace Puissance4
 {
-    class CompteBancaire
+    class Calculatrice
     {
-        public string nomTitulaire = "SALVADOR";
-        public float solde = 0;
-        public void Deposer(float montant)
-        {
-            solde += montant;
+        // une fonction qui prend deux entiers en paramètres
+        // et qui retourne le produit de ces deux entiers
+       public int Multiplier (int a, int b) {
+            return a * b;
         }
-        public void Retirer(float montant)
-        {
-            if (montant > solde)
-            {
-                WriteLine ("Le solde est insuffisant");
-            }
-            else
-            {
-                solde -= montant;
-            }
-        }
-        static void Main(string[] args)
-        {
-            CompteBancaire monCompte = new CompteBancaire();
-            WriteLine("Nom du titulaire du compte : " + monCompte.nomTitulaire);
-            WriteLine("Solde du compte : " + monCompte.solde);
-            WriteLine("Combien voulez-vous déposer ?");
-            monCompte.Deposer(Convert.ToInt32(ReadLine()));
-            WriteLine("Nouveau solde après dépôt : " + monCompte.solde);
-            WriteLine("Combien voulez-vous retirer ?");
-            monCompte.Retirer(Convert.ToInt32(ReadLine()));
-            WriteLine("Nouveau solde après retrait : " + monCompte.solde);
+       public int Additionner (int a, int b) {
+            return a + b;
         }
     }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creer une instance de calculatrice
+            Calculatrice casio = new Calculatrice();
+            // stocker le resultat d'une multiplication dans une variable
+            int produit = casio.Multiplier(5, 7);
+            // afficher ce resultat en console 
+            WriteLine(produit);
+            
+            int somme = casio.Additionner(5, 7);
+            WriteLine(somme);
+        }
+    }
+
 }
