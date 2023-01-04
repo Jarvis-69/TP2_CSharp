c0e2877470aeb5f626c49fa49794cef3508987fd
Jarvis-69 <94354305+Jarvis-69@users.noreply.github.com>
2023-01-04 17:24:17 +0100

    Exo6

--- Program.cs
+++ Program.cs
@@ -3,28 +3,48 @@ using System;
 
 namespace Puissance4
 {
-    class Rectangle {
-        float largeur;
-        float Hauteur;
-        public float Aire(float largeur, float Hauteur){
-            return largeur * Hauteur;
+    // Créez une interface IVolant avec une méthode Voler() qui retourne une chaîne de caractères "Je suis en train de voler!".
+
+    // Créez une classe Oiseau qui implémente l'interface IVolant. La classe Oiseau doit avoir un champ nom (string) et une méthode Description() qui retourne une chaîne de caractères "Je suis un oiseau de nom nom."
+
+    // Créez une classe Avion qui hérite de la classe Oiseau et qui implémente également l'interface IVolant. La classe Avion doit avoir un champ nbMoteurs (int) et une méthode Description() qui retourne une chaîne de caractères "Je suis un avion avec nbMoteurs moteurs de nom nom."
+
+    interface IVolant
+    {
+        string voler();
+    }
+    class Oiseau : IVolant
+    {       
+        public string nom = "Faucon";
+        public string voler() 
+        {
+            return "Je suis en train de voler!";
         }
-        public float Perimetre(float largeur, float Hauteur){
-            return largeur * 2 + Hauteur * 2;
+        public string Description()
+        {
+            return $"Je suis un oiseau de nom {this.nom}.";
         }
     }
 
+    class Avion : Oiseau
+    {
+        int nbMoteurs = 5;
+        public string voler() 
+        {
+            return "Je suis en train de voler!";
+        }
+        public string Description()
+        {
+            return $"Je suis un avion avec {this.nbMoteurs} moteurs de nom nom.";
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Rectangle rectangle = new Rectangle();
-
-            float aire = rectangle.Aire(7,5);
-            WriteLine(aire);
-
-            float perimetre = rectangle.Perimetre(7,5);
-            WriteLine(perimetre);
+            Oiseau oiseau = new Oiseau();
+            Avion avion = new Avion ();
         }
     }
 
