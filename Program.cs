93c5396a9c73180c8acf2b566bc76a67f2d5551a
Jarvis-69 <94354305+Jarvis-69@users.noreply.github.com>
2023-01-04 11:43:13 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+namespace Puissance4
+{
+    class JeuPuissance4
+    {
+        static void Main(string[] args)
+        {
+            string marque = "Tesla";
+            string modele = "Model 3";
+            int nbPortes = 5;
+            string couleur = "Noir";
+            WriteLine($"La voiture est une {marque} modèle {modele} avec {nbPortes} portes de couleur {couleur}.");
+        }
+    }
+}
